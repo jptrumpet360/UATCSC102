3722e6c16d4820571b4cddae439b0d8b9b0529f6
jptrumpet360 <113075688+jptrumpet360@users.noreply.github.com>
2022-10-02 23:45:04 -0500

    Add files via upload

---
+++ week4.js
@@ -0,0 +1,30 @@
+// Alert function
+function eggAlert(){
+  // Alert message
+    alert("This code is located in a separate js file called week4.js.") 
+}
+//Countdown function
+function countofdown(pageElement){
+  // Variable Definition
+    var currTime = 10;
+    // currTime decrement loop
+    for(currTime = 10; currTime > -1; currTime--){
+  // HTML Element
+    pageElement.innerHTML = currTime;
+    //currTime = currTime - 1
+    // Halfway warning If statement
+    
+   }
+   if(currTime < 5) {
+    alert("Warning! Less than halfway to launch! " + currTime );
+   }
+  
+    
+    // Blast Off If statement
+    if(currTime < 1) {
+        alert("Blast Off!");
+    }
+    
+    }
+
+    
